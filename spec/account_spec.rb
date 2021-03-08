8f7ccc8666b46b473fecc2b70f68572c1977ca08
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-08 12:13:24 +0000

    account starts with balance of 0

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,9 @@
-# require 'account'
-#
-# describe Account do
-#
-# end
+require 'account'
+
+describe Account do
+  subject(:account) { described_class.new }
+
+  it 'starts with 0 balance' do
+    expect(account.balance).to eq(0)
+  end
+end
