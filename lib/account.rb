50db44ba270fa5708a71abc0fd99c381a370f77b
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-08 15:36:55 +0000

    refactoring with def negative_amount

--- lib/account.rb
+++ lib/account.rb
@@ -8,17 +8,23 @@ attr_reader :balance
   end
 
   def deposit(amount)
-    raise 'Please enter a positive amount.' if amount < 0
+    negative_amount(amount)
     @balance += amount
   end
 
   def withdraw(amount)
-    raise 'Please enter a positive amount.' if amount < 0
+    negative_amount(amount)
     raise 'There are insufficient funds in your account.' unless @balance > amount
     @balance -= amount
   end
 
   def print_statement
     "date || credit || debit || balance\n8/3/2021 || 10.00 || || 10.00"
+  end
+
+  private
+
+  def negative_amount(amount)
+    raise 'Please enter a positive amount.' if amount < 0
   end
 end
